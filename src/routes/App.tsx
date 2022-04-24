4da125b4e6e7ab373afac0500e953b3cdff83aaa
Irving Juárez <irvingjuarez274@gmail.com>
2022-04-23 19:29:26 -0500

    Modularization of the code

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -3,24 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Home } from "../pages/Home";
 
-import { API } from "../globals"
+import { fetchItems } from "../utils/fetchItems"
 
 const App: React.FC = (): JSX.Element => {
-  useEffect(() => {
-    fetch(API)
-      .then(res => res.json())
-      .then(data => {
-        const requests = data.results.map(
-          (result: {url: string}) => fetch(result.url)
-        )
-        return Promise.all(requests)
-      })
-      .then(promises => {
-        const solvedPromises = promises.map(promise => promise.json())
-        return Promise.all(solvedPromises)
-      })
-      .then(results => console.log(results))
-  })
+  useEffect( () => fetchItems() )
 
   return(
     <BrowserRouter>
---
+++ src/utils/fetchItems.ts
@@ -0,0 +1,17 @@
+import { API } from "../globals"
+
+export const fetchItems = () => {
+  fetch(API)
+    .then(res => res.json())
+    .then(data => {
+      const requests = data.results.map(
+        (result: {url: string}) => fetch(result.url)
+      )
+      return Promise.all(requests)
+    })
+    .then(promises => {
+      const solvedPromises = promises.map(promise => promise.json())
+      return Promise.all(solvedPromises)
+    })
+    .then(results => console.log(results))
+}
