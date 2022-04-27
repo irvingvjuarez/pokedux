fecddc69ab5ec1098b8ab32f505ff82c2021a179
Irving Juárez <irvingjuarez274@gmail.com>
2022-04-27 14:14:55 -0500

    Heade first iteration already done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,18 @@
+import Logo from "../../assets/logo.png"
+import SearchIcon from "../../assets/search.svg"
+
 const Header: React.FC = (): JSX.Element => {
   return(
     <header className="header">
       <div className="header__wrapper">
-        I am the header
+        <div className="header__branding">
+          <img src={Logo} alt="logo" className="header__logo" />
+          <h2 className="header__title">Pokedux</h2>
+        </div>
+
+        <button className="header__search">
+          <img src={SearchIcon} alt="" />
+        </button>
       </div>
     </header>
   )
