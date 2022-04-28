60fd87197978f3851c56a96fb28f8e54e8907136
Irving Juárez <irvingjuarez274@gmail.com>
2022-04-28 10:21:40 -0500

    observer partially done

--- src/containers/Pokemons/index.tsx
+++ src/containers/Pokemons/index.tsx
@@ -1,19 +1,24 @@
 import { AppContext } from "../../context/AppContext"
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useRef } from "react"
 import { IInitialState } from "../../types"
 import { Pokemon } from "../../components/Pokemon"
 import { fetchItems } from "../../utils/fetchItems"
+import { observer } from "../../utils/observer"
 
 const Pokemons: React.FC = (): JSX.Element => {
   const initialState = useContext(AppContext) as IInitialState
   const { state:{pokemons} } = initialState
+  const containerRef = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
-    fetchItems(initialState)
+    // fetchItems(initialState)
+    observer.observe(containerRef.current as HTMLDivElement)
   }, [])
 
   return(
-    <div className="pokemons-container">
+    <div
+      className="pokemons-container"
+      ref={containerRef} >
       {pokemons.map(pokemon => (
         <Pokemon
           key={pokemon.id}
--- src/utils/observer.ts
+++ src/utils/observer.ts
@@ -1,6 +1,12 @@
-const callback = () => console.log("Hello World")
+const callback: IntersectionObserverCallback = (entries) => {
+  const el = entries[0]
+  if(el.intersectionRatio >= 1){
+    console.log("Trigger something here")
+  }
+}
 const config = {
-  threshold: 1.0
+  threshold: 1.0,
+  rootMargin: "0px",
 }
 
 export const observer = new IntersectionObserver(callback, config)
