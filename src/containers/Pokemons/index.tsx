af7443a39fe7af87485a8b6bc66ccf4f1f9bdac7
Irving Juárez <irvingjuarez274@gmail.com>
2022-04-30 21:30:02 -0500

    Everytime the api changes, there will be another observer, but this should be a singleton. I need to turn it into a singleton

--- src/containers/Pokemons/index.tsx
+++ src/containers/Pokemons/index.tsx
@@ -9,14 +9,14 @@ import { useObserver } from "../../hooks/useObserver"
 
 const Pokemons: React.FC = (): JSX.Element => {
   const initialState = useContext(AppContext) as IInitialState
-  const { state:{pokemons}, addPokemons } = initialState
+  const { state:{pokemons, api}, addPokemons } = initialState
   const visorRef = useRef<HTMLDivElement | null>(null)
-  const observer = useObserver(addPokemons)
+  const observer = useObserver(addPokemons, api)
 
   useEffect(() => {
     if(!pokemons.length) fetchItems(initialState)
     observer.observe(visorRef.current as HTMLDivElement)
-  }, [])
+  }, [api])
 
   return(
     <Fragment>
--- src/hooks/useObserver.ts
+++ src/hooks/useObserver.ts
@@ -2,10 +2,10 @@ const config = {
   rootMargin: "0px",
 }
 
-const useObserver = (addPokemons: unknown) => {
+const useObserver = (addPokemons: unknown, RequestApi: string) => {
   return new IntersectionObserver((entries, observer) => {
     const visor = entries[0]
-    if(visor.isIntersecting) console.log("Is Intersecting!")
+    if(visor.isIntersecting) console.log(RequestApi)
   }, config)
 }
 
