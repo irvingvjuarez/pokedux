aa28020dc4e7a62eb0f8e1a95f51a7162852638e
Irving Juárez <irvingjuarez274@gmail.com>
2022-04-30 21:41:31 -0500

    useObserver custom hook is an singleton right now

--- src/hooks/useObserver.ts
+++ src/hooks/useObserver.ts
@@ -2,11 +2,29 @@ const config = {
   rootMargin: "0px",
 }
 
+class Observer {
+  protected constructor(
+    protected addPokemons: unknown
+  ){}
+
+  static instance: IntersectionObserver
+  static RequestApi: string
+  
+  static init(){
+    if(!Observer.instance){
+      Observer.instance = new IntersectionObserver(entries => {
+        const visor = entries[0]
+        if(visor.isIntersecting) console.log(Observer.RequestApi)
+      }, config)
+    }
+
+    return Observer.instance
+  }
+}
+
 const useObserver = (addPokemons: unknown, RequestApi: string) => {
-  return new IntersectionObserver((entries, observer) => {
-    const visor = entries[0]
-    if(visor.isIntersecting) console.log(RequestApi)
-  }, config)
+  Observer.RequestApi = RequestApi
+  return Observer.init()
 }
 
 export { useObserver }
