92decc707ae51a93f339a6f0010f04477470a34a
Irving Juárez <irvingjuarez274@gmail.com>
2022-05-04 18:31:11 -0500

    sidebar improvements in the design

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,23 +1,31 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Link, useLocation } from "react-router-dom"
 
 const Sidebar: React.FC = (): JSX.Element => {
+  const location = useLocation()
+  const [path, setPath] = useState(location.pathname)
+
+  useEffect(() => {
+    setPath(location.pathname)
+  }, [location.pathname])
+
   return(
     <nav className="sidebar">
       <ul>
-        <li>
+        <li className={`sidebar__item ${path === "/" && "active"}`}>
           <Link to="/" >Pokemons</Link>
         </li>
-        <li>
-          <Link to="/" >Items</Link>
+        <li className={`sidebar__item ${path === "/items" && "active"}`}>
+          <Link to="/items" >Items</Link>
         </li>
-        <li>
-          <Link to="/" >Machines</Link>
+        <li className={`sidebar__item ${path === "/machines" && "active"}`}>
+          <Link to="/machines" >Machines</Link>
         </li>
-        <li>
-          <Link to="/" >Locations</Link>
+        <li className={`sidebar__item ${path === "/locations" && "active"}`}>
+          <Link to="/locations" >Locations</Link>
         </li>
-        <li>
-          <Link to="/" >Moves</Link>
+        <li className={`sidebar__item ${path === "/moves" && "active"}`}>
+          <Link to="/moves" >Moves</Link>
         </li>
       </ul>
     </nav>
