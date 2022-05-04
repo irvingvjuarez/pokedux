d25a3b4d3002d4e92c89127272ca241816ca873e
Irving Juárez <irvingjuarez274@gmail.com>
2022-05-04 16:43:18 -0500

    Sidebar added to the mobile version

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import Logo from "../../assets/logo.png"
+import Hamburger from "../../assets/menu.png"
 import SearchIcon from "../../assets/search.svg"
+import { Sidebar } from "../Sidebar"
 
 const Header: React.FC = (): JSX.Element => {
+  const [isSidebar, setIsSidebar] = useState(false)
+  const handleSidebar = () => setIsSidebar(prev => !prev)
+
   return(
     <header className="header">
       <div className="header__wrapper">
@@ -11,10 +17,22 @@ const Header: React.FC = (): JSX.Element => {
           <h2 className="header__title">Pokedux</h2>
         </Link>
 
-        <button className="header__search">
-          <img src={SearchIcon} alt="" />
-        </button>
+        <div className="header__buttons">
+          <button className="header__search">
+            <img src={SearchIcon} alt="" />
+          </button>
+
+          <button className="header__menu" onClick={handleSidebar}>
+            <img src={Hamburger} alt="" />
+          </button>
+        </div>
       </div>
+
+      {isSidebar && (
+        <div className="header__sidebar">
+          <Sidebar />
+        </div>
+      )}
     </header>
   )
 }
