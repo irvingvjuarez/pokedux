dee4218ded99e09cc2e7b7a90711f990d262743a
Irving Juárez <irvingjuarez274@gmail.com>
2022-05-06 19:10:53 -0500

    The slider works now, but I need to fix two main features yet

--- src/pages/PokemonDetail/Carousel/index.tsx
+++ src/pages/PokemonDetail/Carousel/index.tsx
@@ -1,5 +1,6 @@
 import { getMainPhotos } from "./utils"
 import { Link } from "react-router-dom"
+import { useState, useRef, useEffect } from "react";
 
 interface CarouselProps {
   title: string;
@@ -8,6 +9,19 @@ interface CarouselProps {
 
 const Carousel: React.FC<CarouselProps> = ({ title, imagesList }): JSX.Element => {
   const photosArr = getMainPhotos(imagesList)
+  const sliderRef = useRef<HTMLUListElement | null>(null)
+  const [slide, setSlide] = useState<number>(0)
+  const handleRight = () => setSlide(prev => prev += 380)
+  const handleLeft = () => setSlide(prev => prev -= 380)
+
+  useEffect(() => {
+    if(sliderRef.current) sliderRef.current.scroll({
+      top: 0,
+      left: slide,
+      behavior: 'smooth'
+    });
+  }, [slide])
+
 
   return(
     <section className="detail-carousel">
@@ -20,11 +34,12 @@ const Carousel: React.FC<CarouselProps> = ({ title, imagesList }): JSX.Element =
       </div>
 
       <div className="detail-carousel__container">
-        <div className="detail-carousel__slider">
+        <div className="detail-carousel__slider" onClick={handleLeft}>
           <button>&laquo;</button>
         </div>
 
         <ul
+          ref={sliderRef}
           className="detail-carousel__list"
           style={{gridTemplateColumns: `repeat(${photosArr.length}, var(--grid-percent))`}} >
 
@@ -36,9 +51,18 @@ const Carousel: React.FC<CarouselProps> = ({ title, imagesList }): JSX.Element =
         </ul>
 
         <div className="detail-carousel__slider">
-          <button>&raquo;</button>
+          <button onClick={handleRight}>&raquo;</button>
         </div>
       </div>
+
+      <ul className="detail-carousel__guides">
+        {photosArr.map((item, index) => (
+          <li
+            id={index.toString()}
+            key={item.title}
+            className="detail-carousel__guide"></li>
+        ))}
+      </ul>
     </section>
   )
 }
