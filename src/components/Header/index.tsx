676fbaefeef52a304a2fbf2fb49cde929c4f0173
Irving Juárez <irvingjuarez274@gmail.com>
2022-05-08 10:44:02 -0500

    Search box works fine in both desktop and mobile right now

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,7 +10,7 @@ const Header: React.FC = (): JSX.Element => {
   const [isSidebar, setIsSidebar] = useState<boolean>(false)
   const [isSearching, setIsSearching] = useState<boolean>(false)
   const handleSidebar = () => setIsSidebar(prev => !prev)
-  const handleSearch = () => setIsSearching(prev => !prev)
+  const handleSearch = () => setTimeout(() => setIsSearching(prev => !prev), isSearching ? 200 : 0)
 
   return(
     <header className="header">
