a3300923028819204b62c35333089cb31a817c44
Irving Juárez <irvingjuarez274@gmail.com>
2022-05-11 12:59:04 -0500

    The sidebar detail feature is already done

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
 import { ALL_SECTIONS } from "../../globals"
-import { doesMatch } from "./utils";
+import { doesMatch, getMatch } from "./utils";
 
 const Sidebar: React.FC = (): JSX.Element => {
   const location = useLocation()
@@ -14,19 +14,22 @@ const Sidebar: React.FC = (): JSX.Element => {
   return(
     <nav className="sidebar">
       <ul>
-        {ALL_SECTIONS.map(section => (
-          <div key={section.title}>
-            <li className={`sidebar__item ${doesMatch(path, section) && "active"}`}>
-              <Link to="/" >{section.title}</Link>
-            </li>
-            {path.includes(section.detail) && (
-              <h4 className="sidebar__item--detail">
-                <span>&raquo;</span>
-                Pikachu
-              </h4>
-            )}
-          </div>
-        ))}
+        {ALL_SECTIONS.map(section => {
+          const detailMatch = getMatch(path, section);
+          return(
+            <div key={section.title}>
+              <li className={`sidebar__item ${doesMatch(path, section) && "active"}`}>
+                <Link to="/" >{section.title}</Link>
+              </li>
+              {detailMatch && (
+                <h4 className="sidebar__item--detail">
+                  <span>&raquo;</span>
+                  {detailMatch}
+                </h4>
+              )}
+            </div>
+          )
+        })}
       </ul>
     </nav>
   )
--- src/components/Sidebar/utils.ts
+++ src/components/Sidebar/utils.ts
@@ -1,7 +1,24 @@
 import { IPageSection } from "../../types";
 
+const createRegExp = (pattern: string) => {
+  return new RegExp(pattern, 'i');
+}
+
 export const doesMatch = (route: string, section: IPageSection) => {
   const { path, detail } = section
   if(route === path || route.includes(detail)) return true
   return false
+}
+
+export const getMatch = (route: string, section: IPageSection) => {
+  const { detail } = section
+
+  const regExpression = createRegExp(`${detail.replace("/", "\/")}[A-z]+`)
+  const matches = route.match(regExpression)
+  
+  if(matches){
+    const match = matches[0].split("/")
+    return match[1]
+  }
+  return ""
 }
