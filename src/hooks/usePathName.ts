02b9d090b9a89518673a830b369b188380fd4d13
Irving Juárez <irvingjuarez274@gmail.com>
2022-05-11 21:40:29 -0500

    The usePath custom hook has been improved

--- src/hooks/usePathName.ts
+++ src/hooks/usePathName.ts
@@ -1,11 +1,11 @@
-const regex = new RegExp(/[a-z]+$/i)
+const regex = new RegExp(/pokemon\/[a-z]+/i)
 
 const usePathName = (location: any) => {
   let pokemonName = null
   const matches = location.pathname.match(regex) as RegExpMatchArray
 
   if(matches[0]){
-    pokemonName = matches[0]
+    pokemonName = matches[0].replace("pokemon/", "")
   }
 
   return pokemonName
--- src/pages/PokemonPhotos/index.tsx
+++ src/pages/PokemonPhotos/index.tsx
@@ -1,6 +1,12 @@
+import { useLocation } from "react-router-dom"
 import { Sidebar } from "../../components/Sidebar"
+import { usePathName } from "../../hooks/usePathName"
 
 const PokemonPhotos: React.FC = (): JSX.Element => {
+  const location = useLocation()
+  const pokemonName = usePathName(location)
+  console.log("PokemonName", pokemonName)
+
   return(
     <section className="page pokemon-photos">
       <Sidebar />
