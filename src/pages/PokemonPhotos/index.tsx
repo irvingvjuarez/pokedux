315deb84136b9a6906968ff8b098778be79513e2
Irving Juárez <irvingjuarez274@gmail.com>
2022-05-11 21:55:00 -0500

    The photos page can fetch the pokemon if this is not available on the actual state

--- src/pages/PokemonPhotos/index.tsx
+++ src/pages/PokemonPhotos/index.tsx
@@ -1,16 +1,35 @@
+import { useContext, useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
 import { Sidebar } from "../../components/Sidebar"
+import { AppContext } from "../../context/AppContext"
+import { useFetchPokemon } from "../../hooks/useFetchPokemon"
 import { usePathName } from "../../hooks/usePathName"
+import { IInitialState, IPokemon } from "../../types"
 
 const PokemonPhotos: React.FC = (): JSX.Element => {
+  const [pokemon, setPokemon] = useState<IPokemon | null>(null)
   const location = useLocation()
   const pokemonName = usePathName(location)
-  console.log("PokemonName", pokemonName)
+  const { state: {pokemons} } = useContext(AppContext) as IInitialState
+
+  useEffect(() => {
+    async function search4Pokemon(){
+      const aspirantToPokemon = pokemons.find(pokemon => pokemon.name === pokemonName) || await useFetchPokemon(pokemonName as string)
+      setPokemon(aspirantToPokemon)
+    }
+
+    search4Pokemon()
+  }, [location.pathname])
 
   return(
     <section className="page pokemon-photos">
       <Sidebar />
-      <h2>I am the photos page</h2>
+
+      {pokemon ? (
+        <h2>We have pokemon!!</h2>
+      ) : (
+        <span>Loading...</span>
+      )}
     </section>
   )
 }
