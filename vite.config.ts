0c923bb94d85b0e658e5d9ef0d41f63be8e692e7
Irving Juárez <irvingjuarez274@gmail.com>
2022-05-27 11:52:50 -0500

    PWA almost there, there are only some errors related to images

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      mode: "development",
+      base: "/",
+      srcDir: "/src",
+      filename: "sw.ts",
+      strategies: "injectManifest",
+      manifest: {
+        name: "Pokedux",
+        short_name: "Pokedux",
+        theme_color: "#fff",
+        icons: [
+          {
+            src: "/icon192.png",
+            type: "image/png",
+            sizes: "192x192"
+          },
+          {
+            src: "/icon512.png",
+            type: "image/png",
+            sizes: "512x512"
+          },
+          {
+            src: "/icon512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "any maskable"
+          }
+        ],
+        start_url: "/",
+        background_color: "#fff",
+        display: "standalone",
+        scope: "/",
+        related_applications: [],
+        prefer_related_applications: false
+      }
+    })
+  ]
 })
